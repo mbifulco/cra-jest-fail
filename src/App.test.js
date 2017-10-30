f73dbbad9a1b27a06717e709ead4e5dc8d801ae2
Mike Bifulco <mbifulco@live.com>
2017-10-30 07:50:04 -0400

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+it('should fail on this test', () => {
+  expect(true).toBe(false);
+})
